70f0ec547aff001725e299ff046fd8aef52368a7
nikelborm <r0075301net.r@yandex.ru>
2022-05-19 23:08:45 +0300

    models

---
+++ backend/src/modules/infrastructure/model/educationalSpace.model.ts
@@ -0,0 +1,58 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+  OneToMany,
+} from 'typeorm';
+import { User } from '.';
+import { UserGroup } from './userGroup.model';
+
+@Entity({ name: 'educational_space' })
+export class EducationalSpace {
+  @PrimaryGeneratedColumn({ name: 'educational_space_id' })
+  id!: number;
+
+  @Column({
+    name: 'name',
+    nullable: false,
+  })
+  name!: string;
+
+  @Column({
+    name: 'description',
+    nullable: true,
+  })
+  description?: string;
+
+  @ManyToOne(() => User, (user) => user.createdEducationalSpaces, {
+    nullable: false,
+  })
+  @JoinColumn({ name: 'created_by_user_id' })
+  createdBy!: User;
+
+  @Column({
+    name: 'created_by_user_id',
+    nullable: false,
+  })
+  createdByUserId!: number;
+
+  @OneToMany(() => UserGroup, (userGroup) => userGroup.educationalSpace)
+  userGroups!: UserGroup[];
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamptz',
+    nullable: false,
+  })
+  createdAt!: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamptz',
+  })
+  updatedAt!: Date;
+}
--- backend/src/modules/infrastructure/model/index.ts
+++ backend/src/modules/infrastructure/model/index.ts
@@ -1,3 +1,4 @@
 export * from './user.model';
 export * from './accessScope.model';
 export * from './userToAccessScope.model';
+export * from './educationalSpace.model';
