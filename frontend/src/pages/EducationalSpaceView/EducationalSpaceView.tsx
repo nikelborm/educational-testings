18e827b057379d2c54c13fac4f5cfb77a6c35ef4
nikelborm <r0075301net.r@yandex.ru>
2022-05-25 23:38:07 +0300

    sync

--- frontend/src/pages/EducationalSpaceView/EducationalSpaceView.tsx
+++ frontend/src/pages/EducationalSpaceView/EducationalSpaceView.tsx
@@ -1,6 +1,9 @@
+import { Table } from 'antd';
+import { SortOrder } from 'antd/lib/table/interface';
 import { useEducationalSpaceBy } from 'hooks';
 import { useEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
+import { RoutesEnum } from 'types';
 
 export function EducationalSpaceView() {
   const [searchParams] = useSearchParams();
@@ -8,7 +11,8 @@ export function EducationalSpaceView() {
 
   const id = parseInt(searchParams.get('id') as string, 10);
   useEffect(() => {
-    if (Number.isNaN(id)) navigate('/account/myEducationalSpaces');
+    if (Number.isNaN(id))
+      navigate(`/account/${RoutesEnum.MY_EDUCATIONAL_SPACES}`);
   }, [id]);
 
   const { educationalSpace } = useEducationalSpaceBy(id);
@@ -18,6 +22,37 @@ export function EducationalSpaceView() {
       id: {educationalSpace?.id} <br />
       name: {educationalSpace?.name} <br />
       description: {educationalSpace?.description} <br />
+      <Table
+        tableLayout="fixed"
+        dataSource={educationalSpace?.userGroups.map((space) => ({
+          ...space,
+          key: space.id,
+        }))}
+        pagination={false}
+        style={{ marginTop: '24px' }}
+        onRow={(record) => ({
+          onClick: () => {
+            navigate(`/account/${RoutesEnum.USER_GROUP_VIEW}?id=${record.id}`);
+          },
+        })}
+        columns={[
+          {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            sorter: (a, b) => a.name.localeCompare(b.name),
+            sortDirections: ['ascend', 'descend'] as SortOrder[],
+            width: '30%',
+          },
+          {
+            title: 'Description',
+            dataIndex: 'description',
+            key: 'description',
+            sortDirections: ['ascend', 'descend'] as SortOrder[],
+            width: '40%',
+          },
+        ]}
+      />
       Full: <pre>{JSON.stringify(educationalSpace, undefined, 4)}</pre> <br />
     </div>
   );
